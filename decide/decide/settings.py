cf0450050de4aba835c6cda2d1d5af60c5154ee1
ferangmor <ferangmor@alum.us.es>
2021-02-12 10:58:44 +0100

    config de heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -168,6 +168,8 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL= "https://egc-decide-ferangmor-examen.herokuapp.com"
+APIS = {}
 try:
     from local_settings import *
 except ImportError:
@@ -183,3 +185,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
